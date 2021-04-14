511f909e9587101d895689d0c27cb1e95e51a808
zzupd <zzupd@naver.com>
2021-04-14 15:01:53 +0900

    0414_자동슬라이드쇼&중지 구현

--- script/script.js
+++ script/script.js
@@ -1,12 +1,44 @@
 $(function(){
 
+
+
+/*//////////자동 슬라이드 구현 ///////////*/
+    var slideGo = setInterval(fnSlide, 3000);
+
+    function fnSlide() {
+        $("#shuttleFrame").animate({
+                "margin-left": "-370px"
+                },
+                800,
+                function(){
+                    $("#shuttleFrame>span:first")
+                        .insertAfter("#shuttleFrame>span:last");
+                    $("#shuttleFrame")
+                        .css("margin-left", "0");
+                }
+            );
+    }
+
+
+/*//////////자동 슬라이드 구현 ///////////*/
+
+
+/*//////////일시정지&시작 버튼 전환///////////*/
     $("#btnArea").mouseover(function(){
         var srcValue = "images/control_play.png";
         $("#pauseBtn").attr("src", srcValue);
+
+        clearInterval(slideGo);
+
     });
+
     $("#btnArea").mouseout(function(){
         var srcValue = "images/control_pause.png";
         $("#pauseBtn").attr("src", srcValue);
+
+        slideGo = setInterval(fnSlide, 3000);
+
     });
+/*//////////일시정지&시작 버튼 전환///////////*/
 
 });
