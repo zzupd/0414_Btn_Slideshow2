38e10f56c6eb4072aed19f6a86690d681ae02593
zzupd <zzupd@naver.com>
2021-04-14 15:41:06 +0900

    0414_슬라이드쇼_버튼구현완료

--- script/script.js
+++ script/script.js
@@ -41,4 +41,42 @@ $(function(){
     });
 /*//////////일시정지&시작 버튼 전환///////////*/
 
+
+/*//////////이전(btnPrev)으로 이동버튼///////////*/
+    $("#btnPrev").click(function(){
+        $("#shuttleFrame")
+                    .css("margin-left", "-370px");
+
+        $("#shuttleFrame>span:last-child")
+                    .insertBefore("#shuttleFrame>span:first-child");
+        $("#shuttleFrame").animate({
+                                    "margin-left": "0"
+                                },
+                                800
+                      );
+
+    });
+
+/*//////////이전(btnPrev)으로 이동버튼///////////*/
+
+
+/*//////////다음(btnNext)으로 이동버튼///////////*/
+    $("#btnNext").click(function(){
+        $("#shuttleFrame").animate({
+                            "margin-left": "-370px"
+                        },
+                        800,
+                        function(){
+                            $("#shuttleFrame>span:first")
+                                .insertAfter("#shuttleFrame>span:last");
+                            $("#shuttleFrame")
+                                .css("margin-left", "0");
+                        }
+                );
+    });
+
+/*//////////다음(btnNext)으로 이동버튼///////////*/
+
+
+
 });
